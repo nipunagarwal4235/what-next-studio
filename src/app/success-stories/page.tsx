49e0198f7197ea4aad547db6f2379dd1034c6410
Nipun Agarwal <nipunagarwal322@gmail.com>
2025-01-23 01:11:56 +0530

    small fix

--- src/app/success-stories/page.tsx
+++ src/app/success-stories/page.tsx
@@ -9,7 +9,6 @@ export default function BlogsPage() {
         <div className="h-screen container mx-auto">
             <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-20 relative z-20 py-6 bg-clip-text text-white">
                 Success Stories
-                &apos;Get ready to be inspired—our success stories are coming soon to show you what&apos;s possible!&apos;
             </h1>
             <p className="text-sm md:text-lg text-center text-neutral-500 max-w-2xl mx-auto py-4">Get ready to be inspired&mdash;our success stories are coming soon to show you what&apos;s possible!</p>
         </div>
